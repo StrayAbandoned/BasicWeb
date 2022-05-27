8c715caaa21aa3226feb4f2f39d56d5d6ae872f9
Anastasia Lap <stray1988@mail.ru>
2022-05-27 11:31:02 +0300

    Product Table

---
+++ lesson1/src/main/java/ru/geekbrains/MyHttpServlet.java
@@ -0,0 +1,69 @@
+package ru.geekbrains;
+
+import ru.geekbrains.product.Product;
+import ru.geekbrains.product.ProductStorage;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/product/*")
+public class MyHttpServlet extends HttpServlet {
+    private ProductStorage productStorage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.productStorage = new ProductStorage();
+        productStorage.insert(new Product("Cucumber", 6));
+        productStorage.insert(new Product("Potato", 5));
+        productStorage.insert(new Product("Onion", 3));
+        productStorage.insert(new Product("Carrot", 4));
+        productStorage.insert(new Product("Apple", 6));
+        productStorage.insert(new Product("Pineapple", 10));
+        productStorage.insert(new Product("Cauliflower", 10));
+        productStorage.insert(new Product("Broccoli", 8));
+        productStorage.insert(new Product("Orange", 6));
+        productStorage.insert(new Product("Kiwi", 11));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<table>");
+        writer.println("<tr>");
+        writer.println("<th>id</th>");
+        writer.println("<th>title</th>");
+        writer.println("<th>cost</th>");
+        writer.println("</tr>");
+        if (req.getPathInfo()==null){
+            for (Product product: productStorage.findAll()){
+                writer.println("<tr>");
+                writer.println(String.format("<td>%d</td>", product.getId()));
+                writer.println(String.format("<td>%s</td>", product.getTitle()));
+                writer.println(String.format("<td>%d</td>", product.getCost()));
+                writer.println("</tr>");
+            }
+
+        } else {
+            for (Product product: productStorage.findAll()){
+                if (req.getPathInfo().equals(String.format("/%d",product.getId()))){
+                    writer.println("<tr>");
+                    writer.println(String.format("<td>%d</td>", product.getId()));
+                    writer.println(String.format("<td>%s</td>", product.getTitle()));
+                    writer.println(String.format("<td>%d</td>", product.getCost()));
+                    writer.println("</tr>");
+                }
+            }
+        }
+
+
+        writer.println("</table>");
+
+
+    }
+}
